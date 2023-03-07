fc2eda8b9d7b4618face8e80531876a65c313a02
Vitaliy <ya.1mfi@yandex.ru>
2023-03-07 20:11:25 +0700

    override posts title

---
+++ README.md
@@ -0,0 +1,1 @@
+override posts edit form
---
+++ app/Voyager/Controllers/PostsController.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Voyager\Controllers;
+
+use TCG\Voyager\Http\Controllers\VoyagerBaseController;
+
+class PostsController extends VoyagerBaseController
+{
+}
